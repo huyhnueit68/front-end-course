db337ee32aa6280ec29ecef1c46609ea01d395a5
harrypham <pham_quanghuy@wohhup.com.vn>
2024-01-25 22:05:25 +0700

    add

--- index.js
+++ index.js
@@ -28,22 +28,30 @@ var listTabs = [
     }
 ];
 
-
+/**
+ * Func display side bar tabs
+ * @param {*} array list tabs
+ * CreatedBy: PQ Huy (25.01.2024)
+ */
 function displayListTabs(array) {
     const outputDiv = document.getElementById('page__sidebar');
-    array.forEach(item => {
+    
+    array.forEach((item, index) => {
         let  appsHTML = '';
 
         item.apps.forEach(app => {
-            appsHTML =  `${appsHTML} 
-            <div class='group-tabs__app'>${app}</div> `;
+            appsHTML =  `${appsHTML} <div class='group-tabs__app'>${app}</div> `;
         })
 
+        // create div tag by js
         const listItem = document.createElement('div');
+        const hor_bar = `<div class='group-hor--bar'></div>`
+
         listItem.innerHTML = `
             <div class='group-tabs'>
                 <div class='group-tabs__title'>${item.titleGroup}</div>
                 ${appsHTML}
+                ${(index + 1) == array?.length ? '' : hor_bar}
             </div>
         `;
         
